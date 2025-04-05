1f9cb7bc28b52c5b54e880d3a872b754113653dc
RDC <basilscott@rocketmail.com>
2025-04-05 18:27:32 -0400

    Reduce Ceramic damage, Increase rubber damage. Undo ERA effective buff.

--- lua/acf/shared/armor/era.lua
+++ lua/acf/shared/armor/era.lua
@@ -13,8 +13,8 @@ Material.curve		= 0.95
 --All effectiveness values multiply the Line of Sight armor values of armor.
 --All Resiliance values are damage multipliers. Higher = more damage. Lower = less damage.
 
-Material.effectiveness	= 3	--5 --Data before to angle factor. Needs proper testing
-Material.HEATeffectiveness  = 10	--20
+Material.effectiveness	= 2.5	--5 --Data before to angle factor. Needs proper testing
+Material.HEATeffectiveness  = 8	--20
 
 Material.resiliance		= 1
 Material.HEATresiliance	= 1

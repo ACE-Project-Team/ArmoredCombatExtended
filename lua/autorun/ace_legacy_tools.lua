2465aa2eb0a2cfb3453364bb8f9dc4daba5d80cd
Zac Yandell <49886626+Ally-for-Hire@users.noreply.github.com>
2026-08-14 13:19:34 -0500

    Cover all compatibility tool phrases

--- lua/autorun/ace_legacy_tools.lua
+++ lua/autorun/ace_legacy_tools.lua
@@ -44,58 +44,78 @@ if CLIENT then
 	-- Master clients can receive canonical tool files from a newer server
 	-- without receiving that server checkout's localization resource.
 	local CompatibilityPhrases = {
-		acemenu = {
-			name = "ACE Menu", desc = "Spawn the Armored Combat Extended weapons and ammo",
-			left = "Create/Update entity", right = "Link/Unlink entities"
+		menu = {
+		name = "ACE Menu", desc = "Spawn the Armored Combat Extended weapons and ammo",
+		left = "Create/Update entity", right = "Link/Unlink entities",
+		stage1 = {
+			link = "Link selected entities to this entity",
+			unlink = "(Hold Use) Unlink selected entities from this entity",
+			multiselect = "(Hold Shift) Select more entities",
+			reload = "Deselect all entities"
 		},
-		acearmorprop = {
+		creationfailed = "Failed to create entity"
+		},
+		armor = {
 			name = "ACE Armor Properties", desc = "Sets the ACE armour of an entity",
 			left = "Apply armour settings", right = "Copy armour settings",
-			reload = "Get information about contraption"
+			reload = "Get information about contraption",
+			reloadhint = "Get information about contraption (double-tap R for preview values)",
+			thickness = "Thickness",
+			thicknessdesc = "Set the desired armor thickness (in mm) and the mass will be adjusted accordingly.",
+			ductility = "Ductility",
+			ductilitydesc = "Set the desired armor ductility (thickness-vs-health bias).",
+			current = "Current", after = "After", mass = "Mass", mass_scale = "Mass Scale",
+			armor = "Armour", health = "Health", material = "Material", acepoints = "Point Cost",
+			armorinfo = "Armour Info", curve = "Curve", keprot = "Kinetic Protection",
+			chemprot = "Chemical Protection", year = "Year", reloadfull = "Shift + Reload: Get full point readout"
 		},
-		acechaircam = {
+		copy = {
+			name = "ACE Copy Tool", desc = "Copy ammo or gearbox data from one object to another",
+			[0] = "Left click to paste data, Right click to copy data",
+			gearbox = "Gearbox copied successfully!", ammo = "Ammo copied successfully!"
+		},
+		sound = {
+			name = "ACE Sound Replacer", desc = "Change the sound of guns and engines",
+			[0] = "Left click to apply sound. Right click to copy sound. Reload to set default sound. Use an empty sound path to disable sound.",
+			left = "Apply the new sound", right = "Copy the sound", reload = "Reset to default sound",
+			unsupported = "This entity does not support sound replacement",
+			info = "Replaces default sounds of certain ACE entities with this tool. You can replace the sounds of cannons, racks, engines and Anti-Missile Radar.",
+			openbrowser = "Open Sound Browser", play = "Play", stop = "Stop", copy = "Copy to Clipboard",
+			clear = "Clear Sound", pitch = "Pitch",
+			pitchdesc = "Adjust the pitch of the sound. Currently supports engines, guns, racks and missile radars."
+		},
+		chaircam = {
 			name = "ACE Third Person View Fixer",
 			desc = "Allows third person view to pass through all type of entities while seated, useful when cam controllers are not used",
 			left = "Apply the fix to a seat", reload = "Remove the fix from a seat"
-		},
-		acecopy = {
-			name = "ACE Copy Tool", desc = "Copy ammo or gearbox data from one object to another",
-			[0] = "Left click to paste data, Right click to copy data"
-		},
-		acesound = {
-			name = "ACE Sound Replacer", desc = "Change the sound of guns and engines",
-			left = "Apply the new sound", right = "Copy the sound",
-			reload = "Reset to default sound"
-		},
-		acfarmorprop = {
-			name = "ACE Armor Properties", desc = "Sets the ACE armour of an entity",
-			left = "Apply armour settings", right = "Copy armour settings",
-			reload = "Get information about contraption"
-		},
-		acfchaircam = {
-			name = "ACE Third Person View Fixer",
-			desc = "Allows third person view to pass through all type of entities while seated, useful when cam controllers are not used",
-			left = "Apply the fix to a seat", reload = "Remove the fix from a seat"
-		},
-		acfcopy = {
-			name = "ACE Copy Tool", desc = "Copy ammo or gearbox data from one object to another",
-			[0] = "Left click to paste data, Right click to copy data"
-		},
-		acfmenu = {
-			name = "ACE Menu", desc = "Spawn the Armored Combat Extended weapons and ammo",
-			left = "Create/Update entity", right = "Link/Unlink entities"
-		},
-		acfsound = {
-			name = "ACE Sound Replacer", desc = "Change the sound of guns and engines",
-			left = "Apply the new sound", right = "Copy the sound",
-			reload = "Reset to default sound"
 		}
 	}
 
-	for mode, phrases in pairs(CompatibilityPhrases) do
+	local ToolPhraseModes = {
+		acemenu = "menu",
+		acfmenu = "menu",
+		acearmorprop = "armor",
+		acfarmorprop = "armor",
+		acechaircam = "chaircam",
+		acfchaircam = "chaircam",
+		acecopy = "copy",
+		acfcopy = "copy",
+		acesound = "sound",
+		acfsound = "sound"
+	}
+
+	local function AddPhrases(mode, phrases, prefix)
 		for phrase, value in pairs(phrases) do
-			language.Add("tool." .. mode .. "." .. phrase, value)
+			if istable(value) then
+				AddPhrases(mode, value, prefix .. phrase .. ".")
+			else
+				language.Add("tool." .. mode .. "." .. prefix .. phrase, value)
+			end
 		end
+	end
+
+	for mode, phraseGroup in pairs(ToolPhraseModes) do
+		AddPhrases(mode, CompatibilityPhrases[phraseGroup], "")
 	end
 
 end
